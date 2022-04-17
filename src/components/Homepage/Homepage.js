b5cfb69790946f8ddd71aa7f4faed2df558f5a4b
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-18 01:32:36 +0500

    :art: Added Basic Working Carousel

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Carousel from './Components/Carousel/Carousel';
-/* <Carousel /> */
 import '../../stylesheets/carousel.css';
 
 const Homepage = () => {
+  const [slide, setSlide] = useState(0);
   const images = [
-    { src: 'https://i.pinimg.com/originals/c2/8b/c9/c28bc99dafaeb107246f904f34bae16c.jpg' },
-    { src: 'https://images.wallpaperscraft.com/image/single/space_galaxy_shine_137572_1280x720.jpg' },
-    { src: 'https://cdn.wallpapersafari.com/74/30/v23HkS.jpg' },
-    { src: 'https://wallpaperaccess.com/full/3971879.jpg' },
+    { id: 1, src: 'https://i.pinimg.com/originals/c2/8b/c9/c28bc99dafaeb107246f904f34bae16c.jpg' },
+    { id: 2, src: 'https://images.wallpaperscraft.com/image/single/space_galaxy_shine_137572_1280x720.jpg' },
+    { id: 3, src: 'https://cdn.wallpapersafari.com/74/30/v23HkS.jpg' },
+    { id: 4, src: 'https://wallpaperaccess.com/full/3971879.jpg' },
   ];
+  const nextSlide = () => setSlide((slide) => {
+    let prev = slide;
+    prev += 1;
+    return prev;
+  });
+  const prevSlide = () => setSlide((slide) => {
+    let next = slide;
+    next -= 1;
+    return next;
+  });
+
   return (
-    <section className="carousel">
-      <div className="carousel__track-container">
-        <button className="carousel__button carousel__button--left" type="button"><span>&larr;</span></button>
-        <ul className="carousel__track">
+    <>
+      <section className="carousel">
+        <div className="carousel__track-container">
           {
+          slide === 0
+            ? ''
+            : (
+              <button
+                onClick={prevSlide}
+                className="carousel__button carousel__button--left"
+                type="button"
+              >
+                <span className="carousel__button--icon">&#10094;</span>
+              </button>
+            )
+        }
+          <ul className="carousel__track">
+            {
             images.map((img, idx) => (
-              <li key={img} className={idx === 0 ? 'carousel__slide current__slide' : 'carousel__slide'}>
+              <li key={img.id} className={idx === slide ? 'carousel__slide current__slide' : 'carousel__slide'}>
                 <img className="carousel__image" src={img.src} alt="carousel snap" />
               </li>
             ))
           }
-        </ul>
-        <button className="carousel__button carousel__button--right" type="button"><span>&rarr;</span></button>
-      </div>
-    </section>
+          </ul>
+          {
+          slide === images.length - 1
+            ? ''
+            : (
+              <button
+                onClick={nextSlide}
+                className="carousel__button carousel__button--right"
+                type="button"
+              >
+                <span className="carousel__button--icon">&#10095;</span>
+              </button>
+            )
+        }
+        </div>
+      </section>
+      {/* <Carousel /> */}
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<BrowserRouter><App /></BrowserRouter>);
