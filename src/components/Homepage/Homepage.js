91b337fda3d0f6c25e3941e2b9fe584c56bfed71
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-25 18:19:02 +0500

    Added Carousel

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -5,10 +5,10 @@ import '../../stylesheets/carousel.css';
 const Homepage = () => {
   const [slide, setSlide] = useState(0);
   const images = [
-    { id: 1, src: 'https://i.pinimg.com/originals/c2/8b/c9/c28bc99dafaeb107246f904f34bae16c.jpg' },
-    { id: 2, src: 'https://images.wallpaperscraft.com/image/single/space_galaxy_shine_137572_1280x720.jpg' },
-    { id: 3, src: 'https://cdn.wallpapersafari.com/74/30/v23HkS.jpg' },
-    { id: 4, src: 'https://wallpaperaccess.com/full/3971879.jpg' },
+    { id: 0, src: 'https://i.pinimg.com/originals/c2/8b/c9/c28bc99dafaeb107246f904f34bae16c.jpg' },
+    { id: 1, src: 'https://images.wallpaperscraft.com/image/single/space_galaxy_shine_137572_1280x720.jpg' },
+    { id: 2, src: 'https://cdn.wallpapersafari.com/74/30/v23HkS.jpg' },
+    { id: 3, src: 'https://wallpaperaccess.com/full/3971879.jpg' },
   ];
   const nextSlide = () => setSlide((slide) => {
     let prev = slide;
@@ -20,6 +20,8 @@ const Homepage = () => {
     next -= 1;
     return next;
   });
+
+  const sliderWidth = window.innerWidth;
 
   return (
     <>
@@ -38,10 +40,10 @@ const Homepage = () => {
               </button>
             )
         }
-          <ul className="carousel__track">
+          <ul className="carousel__track" style={{ transform: `translateX(-${sliderWidth * slide}px)` }}>
             {
             images.map((img, idx) => (
-              <li key={img.id} className={idx === slide ? 'carousel__slide current__slide' : 'carousel__slide'}>
+              <li key={img.id} style={{ left: sliderWidth * idx }} className={idx === slide ? 'carousel__slide current__slide' : 'carousel__slide'}>
                 <img className="carousel__image" src={img.src} alt="carousel snap" />
               </li>
             ))
@@ -62,7 +64,6 @@ const Homepage = () => {
         }
         </div>
       </section>
-      {/* <Carousel /> */}
     </>
   );
 };
